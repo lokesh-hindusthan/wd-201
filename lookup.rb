fcead69c05f80f7c1d932edaff6fa32d6d3aba63
lokesh-hindusthan <90122506+lokesh-hindusthan@users.noreply.github.com>
2021-09-16 21:25:09 +0530

    Update lookup.rb

--- lookup.rb
+++ lookup.rb
@@ -1,7 +1,7 @@
 def get_command_line_argument
   # ARGV is an array that Ruby defines for us,
   # which contains all the arguments we passed to it
-  # when invoking the  script from the command line.
+  # when invoking the script from the command line.
   # https://docs.ruby-lang.org/en/2.4.0/ARGF.html
   if ARGV.empty?
     puts "Usage: ruby lookup.rb <domain>"
@@ -19,29 +19,37 @@ domain = get_command_line_argument
 
 dns_raw = File.readlines("zone")
 
-def parse_dns(rawinput)
-  knownrecords = {}
-  rawinput.
-    reject { |line| line.empty? || line[0] == "#" }.
-    map { |line| line.strip.split(", ") }.
-    reject do |resolverecord|
+def parse_dns(dnsstring)
+  # creating empty hash using {}
+  inputrecords = {}
+  # use "reject" with OR condition that can used to remove the empty lines of dnsstring || to remove first hash line of the input received form the user
+  # To remove the empty line "dnsstring=dnsstring.reject { |s| s.strip.empty? }" this statement may be used.
+  # To remove first line "dnsstring=dnsstring[1..-1]" this statement may be used.
+  dnsstring.
+    reject { |s| s.strip.empty? || s.include?("#") }.
+    map { |s| s.strip.split(", ") }.
+    reject do |dnsrecord|
   end.
-    each_with_object({}) do |resolverecord, knownrecords|
-     knownrecords[resolverecord[1]] = { type: resolverecord[0], target: resolverecord[2] }
+    each_with_object({}) do |dnsrecord, inputrecords|
+    inputrecords[dnsrecord[1]] = { type: dnsrecord[0], target: dnsrecord[2] }
   end
 end
 
 def resolve(dns_records, lookup_chain, domain_name)
-  resolverecord = dns_records[domain_name]
-  if resolverecord[:type] == "CNAME"
-    lookup_chain <<  resolverecord[:target]
-    resolve(dns_records, lookup_chain,  resolverecord[:target])
-  elsif  resolverecord[:type] == "A"
-    lookup_chain <<  resolverecord[:target]
-  elsif (! resolverecord)
-    lookup_chain << "Error: Record not found for " + domain_name
+  dnsrecord = dns_records[domain_name]
+  # To check dnsrecord is valid domain name or error record
+  if (!dnsrecord)
+    lookup_chain=[]
+    displayerror1 = "Error: record not found for "+ domain_name
+    lookup_chain.push(displayerror1)
+  elsif dnsrecord[:type] == "A"
+    lookup_chain.push(dnsrecord[:target])
+  elsif dnsrecord[:type] == "CNAME"
+    lookup_chain.push(dnsrecord[:target])
+    resolve(dns_records, lookup_chain, dnsrecord[:target])
   else
-    lookup_chain << "Invalid record type for " + domain_name
+    displayerror2 = "Invalid record type for " + domain_name
+    lookup_chain.push(displayerror2)
   end
 end
 
