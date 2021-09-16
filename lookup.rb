9b342ee8da4df7d9cf8e3ec7307d3afe28cd50f4
lokesh-hindusthan <90122506+lokesh-hindusthan@users.noreply.github.com>
2021-09-16 10:38:25 +0530

    Update lookup.rb

--- lookup.rb
+++ lookup.rb
@@ -22,7 +22,7 @@ dns_raw = File.readlines("zone")
 def parse_dns(rawinput)
   knownrecords = {}
   rawinput.
-    reject { |line| line.strip.empty? || line.include?("#") }.
+    reject { |line| line.empty? || line[0] == "#" }.
     map { |line| line.strip.split(", ") }.
     reject do |resolverecord|
   end.
