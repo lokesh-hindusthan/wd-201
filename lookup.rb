c4063f4bd6cc5abffba379e2c2f910a28881c2f8
lokesh-hindusthan <90122506+lokesh-hindusthan@users.noreply.github.com>
2021-09-17 20:29:20 +0530

    Update lookup.rb

--- lookup.rb
+++ lookup.rb
@@ -16,42 +16,44 @@ domain = get_command_line_argument
 # File.readlines reads a file and returns an
 # array of string, where each element is a line
 # https://www.rubydoc.info/stdlib/core/IO:readlines
-
 dns_raw = File.readlines("zone")
 
-def parse_dns(dnsstring)
-  # creating empty hash using {}
-  inputrecords = {}
-  # use "reject" with OR condition that can used to remove the empty lines of dnsstring || to remove first hash line of the input received form the user
-  # To remove the empty line "dnsstring=dnsstring.reject { |s| s.strip.empty? }" this statement may be used.
-  # To remove first line "dnsstring=dnsstring[1..-1]" this statement may be used.
-  dnsstring.
-    reject { |s| s.strip.empty? || s.include?("#") }.
-    map { |s| s.strip.split(", ") }.
-    reject do |dnsrecord|
-  end.
-    each_with_object({}) do |dnsrecord, inputrecords|
-    inputrecords[dnsrecord[1]] = { type: dnsrecord[0], target: dnsrecord[2] }
-  end
-end
 
-def resolve(dns_records, lookup_chain, domain_name)
-  dnsrecord = dns_records[domain_name]
-  # To check dnsrecord is valid domain name or error record
-  if (!dnsrecord)
+# parse_dns to return dns as records - hash
+def parse_dns(dns_string)
+    #remove empty lines
+  dns_string = dns_string.map(&:strip).delete_if { |string| string.length == 0 }
+    # removing first hash line
+  dns_string=dns_string[1..-1]
+  arr=Array.new(5) {Array.new(3) }
+  input=[]
+  for x in 0..4
+  input=dns_string[x].strip.split(",")
+    for y in 0..2
+      arr[x][y]=input[y].strip
+    end
+  end
+ h= Hash[arr.map { |key,d1,d2| [d1,{:type=>key,:target=>d2}]}]
+ end
+
+# dns resolver
+def resolve(dns_records,lookup_chain,domain_name)
+  record=dns_records[domain_name]
+  if !(record)
     lookup_chain=[]
     displayerror1 = "Error: record not found for "+ domain_name
     lookup_chain.push(displayerror1)
-  elsif dnsrecord[:type] == "A"
-    lookup_chain.push(dnsrecord[:target])
-  elsif dnsrecord[:type] == "CNAME"
-    lookup_chain.push(dnsrecord[:target])
-    resolve(dns_records, lookup_chain, dnsrecord[:target])
+  elsif record[:type]=="A"
+    lookup_chain.push(record[:target])
+  elsif record[:type]=="CNAME"
+    lookup_chain.push(record[:target])
+    resolve(dns_records,lookup_chain,record[:target])
   else
     displayerror2 = "Invalid record type for " + domain_name
     lookup_chain.push(displayerror2)
   end
 end
+
 
 dns_records = parse_dns(dns_raw)
 lookup_chain = [domain]
